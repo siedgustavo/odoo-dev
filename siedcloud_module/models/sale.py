fc19a9a139e83a4916838c8dd97614eaa096c47e
siedgustavo <siedgustavo@gmail.com>
2021-12-20 17:27:10 -0300

    finalized button

--- siedcloud_module/models/sale.py
+++ siedcloud_module/models/sale.py
@@ -7,3 +7,7 @@ class Sale(models.Model):
      customer_id = fields.Many2one(comodel_name="customer", string="Cliente", required=True)
      product_ids = fields.Many2many("product", string="Productos")
      state = fields.Selection([('pending','Pendiente'),('finalized','Finalizada')])
+
+
+     def boton_finalizar(self):
+          self.state = "finalized"
